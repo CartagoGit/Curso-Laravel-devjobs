2914d7163d3ceb03870493c2958b7f293588a950
Cartago <cartago.relaxingcup@gmail.com>
2023-07-29 17:19:35 +0200

    Instalamos breeze para blade

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        hmr: {
-            host: process.env.APP_URL,
-            log: true,
-        },
-    },
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
