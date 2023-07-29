6d204aaff1349a8e4c880a0dc142b70ba0fdc852
Cartago <cartago.relaxingcup@gmail.com>
2023-07-29 18:25:32 +0200

    Realizamos cambios en la app_url y con las variables de entorno

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+import { config } from "dotenv";
+// require("dotenv").config();
+config();
 
 export default defineConfig({
+    server: {
+        hmr: {
+            // host: process.env.APP_URL || "localhost",
+            host: process.env.APP_URL,
+            // host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
