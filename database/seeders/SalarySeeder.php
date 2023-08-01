4a4bb306ffaf59b41654da43c61a7affd5325cc2
Cartago <cartago.relaxingcup@gmail.com>
2023-08-01 21:03:15 +0200

    Añadimos los datos del seeder y creamos la migracion para salarios, y realizamos la migracion

--- database/seeders/SalarySeeder.php
+++ database/seeders/SalarySeeder.php
@@ -4,14 +4,70 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SalarySeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('salarys')->insert([
+            'salary' => '$0 - $499',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('salarys')->insert([
+            'salary' => '$500 - $749',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('salarys')->insert([
+            'salary' => '$750 - $999',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('salarys')->insert([
+            'salary' => '$1000 - $1499',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('salarys')->insert([
+            'salary' => '$1500 - $1999',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('salarys')->insert([
+            'salary' => '$2000 - $2499',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('salarys')->insert([
+            'salary' => '$2500 - $2999',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('salarys')->insert([
+            'salary' => '$3000 - $4999',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('salarys')->insert([
+            'salary' => '+$5000',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
     }
 }
