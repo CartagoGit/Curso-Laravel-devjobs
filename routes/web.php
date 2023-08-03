538087c11d6fd961d7bf26a7ad4b20044ced0291
Cartago <cartago.relaxingcup@gmail.com>
2023-08-03 20:47:08 +0200

    Añadimos las rutas de vacantes a usuarios autenticados

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/vacants',[VacantController::class, 'index'])->middleware(['auth', 'verified'])->name('vacants.index');
-Route::get('/vacants/create',[VacantController::class, 'create'])->middleware(['auth', 'verified'])->name('vacants.create');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/vacants',[VacantController::class, 'index'])->middleware(['auth', 'verified'])->name('vacants.index');
+    Route::get('/vacants/create',[VacantController::class, 'create'])->middleware(['auth', 'verified'])->name('vacants.create');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
