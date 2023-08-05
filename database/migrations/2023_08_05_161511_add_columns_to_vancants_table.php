012b5ac1c62096a523994c5264a41eab88bdbd5a
Cartago <cartago.relaxingcup@gmail.com>
2023-08-05 18:23:22 +0200

    Creamos los campos que se añadiran en la nueva migracion con las nuevas columnas

--- database/migrations/2023_08_05_161511_add_columns_to_vancants_table.php
+++ database/migrations/2023_08_05_161511_add_columns_to_vancants_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::table('vancants', function (Blueprint $table) {
             //
+            $table->string('title');
+            $table->foreignId('salary_id')->constrained('salaries')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->string('company');
+            $table->date('last_day_of_vacant');
+            $table->text('description');
+            $table->string('image');
+            $table->integer('published')->default(1);
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
         });
     }
 
@@ -22,7 +31,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('vancants', function (Blueprint $table) {
-            //
+
+            $table->dropColumn(['title', 'salary_id', 'category_id', 'company', 'last_day_of_vacant', 'description', 'image', 'published', 'user_id']);
         });
     }
 };
