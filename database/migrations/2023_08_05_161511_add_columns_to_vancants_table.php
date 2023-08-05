f698acf9454b4857e1a25d95538813f5ec70db99
Cartago <cartago.relaxingcup@gmail.com>
2023-08-05 18:16:28 +0200

    Quitamos lo que habiamos añadido en la migracion anterior y creamos una nueva migracion donde lo añadiremos

--- database/migrations/2023_08_05_161511_add_columns_to_vancants_table.php
+++ database/migrations/2023_08_05_161511_add_columns_to_vancants_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('vacants', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('vancants', function (Blueprint $table) {
+            //
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('vacants');
+        Schema::table('vancants', function (Blueprint $table) {
+            //
+        });
     }
 };
