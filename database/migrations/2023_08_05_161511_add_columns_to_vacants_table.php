e8f301ece268935fc1d1daa7b012a662525e3e30
Cartago <cartago.relaxingcup@gmail.com>
2023-08-09 20:20:54 +0200

    Modificamos la migracion apra que el array con las tablas y anotacion sobre ello

--- database/migrations/2023_08_05_161511_add_columns_to_vacants_table.php
+++ database/migrations/2023_08_05_161511_add_columns_to_vacants_table.php
@@ -31,8 +31,23 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('vacants', function (Blueprint $table) {
-            $table->dropForeign(['salary_id', 'category_id', 'user_id']);
-            $table->dropColumn(['title', 'salary_id', 'category_id', 'company', 'last_day_of_vacant', 'description', 'image', 'published', 'user_id']);
+            //* Eliminar las relaciones antes de nada
+            $table->dropForeign([
+                'salary_id',
+                'category_id',
+                'user_id'
+            ]);
+            $table->dropColumn([
+                'title',
+                'salary_id',
+                'category_id',
+                'company',
+                'last_day_of_vacant',
+                'description',
+                'image',
+                'published',
+                'user_id'
+            ]);
         });
     }
 };
