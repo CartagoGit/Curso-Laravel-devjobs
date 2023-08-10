020d17a56ef87f5b01e185b5ced395b1db2af43c
Cartago <cartago.relaxingcup@gmail.com>
2023-08-10 13:11:36 +0200

    En el modelo de vacantes añadimos los campos que vamos a rellenar al crear la vacante

--- app/Models/Vacant.php
+++ app/Models/Vacant.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Vacant extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+
+        'title',
+        'salary_id',
+        'category_id',
+        'company',
+        'last_day_of_vacant',
+        'description',
+        'image',
+        'published',
+        'user_id'
+
+    ];
 }
